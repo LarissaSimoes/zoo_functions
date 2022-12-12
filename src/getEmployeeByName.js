2fb3849604527ccf1665dcaa3e5d72cf988a5ea2
LarissaSimoes <larissacarneiro2@hotmail.com>
2022-12-12 17:07:29 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  if (employeeName === undefined) return {};
+
+  const employeeFound = data.employees.find((person) => person.firstName === employeeName
+  || person.lastName === employeeName);
+
+  return employeeFound;
 };
 
 module.exports = getEmployeeByName;
