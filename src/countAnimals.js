34f1eb12b8e3099c25630eea913a646981acaa7d
LarissaSimoes <larissacarneiro2@hotmail.com>
2022-12-15 15:59:07 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (animal === undefined) {
+    return species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  const animalName = species.find((specie) => (specie.name === animal.species) && specie);
+  const animalGender = animalName.residents.filter(({ sex }) => sex === animal.sex);
+
+  if (animal.sex) {
+    return animalGender.length;
+  }
+  return animalName.residents.length;
 };
 
 module.exports = countAnimals;
