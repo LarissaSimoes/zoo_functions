623f080123fea9e84dc78f79b25e74a2f41af2f4
LarissaSimoes <larissacarneiro2@hotmail.com>
2022-12-15 21:30:35 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  if (!entrants) return 0;
+  const children = entrants.filter((persons) => persons.age < 18).length;
+
+  const adults = entrants.filter((persons) => persons.age >= 18 && persons.age < 50).length;
+
+  const seniors = entrants.filter((persons) => persons.age >= 50).length;
+
+  return { child: children, adult: adults, senior: seniors };
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (entrants === undefined || entrants.length === undefined) return 0;
+  const childSum = countEntrants(entrants).child * 20.99;
+  const adultSum = countEntrants(entrants).adult * 49.99;
+  const seniorSum = countEntrants(entrants).senior * 24.99;
+  return childSum + adultSum + seniorSum;
 };
 
 module.exports = { calculateEntry, countEntrants };
