ccd659f996c78b96886edd9c086bea9413d05fad
LarissaSimoes <larissacarneiro2@hotmail.com>
2022-12-19 15:51:17 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const animalFound = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const animalObj = data.species.find((specie) => specie.id === animalFound).residents;
+  return Object.values(animalObj.sort((a, b) => a.age - b.age)[animalObj.length - 1]);
 };
 
 module.exports = getOldestFromFirstSpecies;
